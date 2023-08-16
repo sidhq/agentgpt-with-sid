d50a85915b56e5f31d449e1089e507ba315006d9
Georgijs Vilums <georgijs.vilums@gmail.com>
2023-08-16 16:48:43 -0700

    fix SID connect and disconnect logic

--- platform/reworkd_platform/web/api/auth/views.py
+++ platform/reworkd_platform/web/api/auth/views.py
@@ -1,12 +1,12 @@
-from typing import Annotated, Dict, List
+from typing import Annotated, Dict, List, Any
 
 from fastapi import APIRouter, Depends, HTTPException, Form
 from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 from slack_sdk import WebClient
 
-from reworkd_platform.db.crud.oauth import OAuthCrud
 from reworkd_platform.db.crud.organization import OrganizationCrud, OrganizationUsers
+from reworkd_platform.db.crud.oauth import OAuthCrud
 from reworkd_platform.schemas import UserBase
 from reworkd_platform.schemas.user import OrganizationRole
 from reworkd_platform.services.oauth_installers import (
@@ -69,6 +69,16 @@ async def oauth_install(
     """Install an OAuth App"""
     return await installer.install(user, redirect)
 
+@router.get("/{provider}/uninstall")
+async def oauth_uninstall(
+    user: UserBase = Depends(get_current_user),
+    installer: OAuthInstaller = Depends(installer_factory),
+) -> Dict[str, Any]:
+    res = await installer.uninstall(user)
+    return {
+        'success': res,
+    }
+
 
 @router.get("/{provider}/callback")
 async def oauth_callback(
@@ -81,6 +91,16 @@ async def oauth_callback(
 
     return RedirectResponse(url=creds.redirect_uri)
 
+@router.get("/sid/info")
+async def sid_info(
+    user: UserBase = Depends(get_current_user),
+    crud: OAuthCrud = Depends(OAuthCrud.inject),
+) -> Dict[str, Any]:
+    creds = await crud.get_installation_by_user_id(user.id, "sid")
+    connected = creds is not None and creds.access_token_enc is not None and creds.access_token_enc != ""
+    return {
+        'connected': connected,
+    }
 
 class Channel(BaseModel):
     name: str
